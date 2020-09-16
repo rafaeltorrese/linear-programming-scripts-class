6ab8637e9f566ab9e510bc332f999b3fb4fb65cf
Rafael Torres Escobar <torresc.rafael@gmail.com>
2020-09-16 16:13:19 -0500

    Method get_solutions was added

--- simplex/03_analytical-method-simplex.py
+++ simplex/03_analytical-method-simplex.py
@@ -12,11 +12,11 @@ class Constraint:
 
     def vector(self):
         'Construct vector from string constraint'
-        vars, rhs =  re.split("[<>]*=", self._expression)
+        vars, rhs =  re.split("[<>]?=", self._expression)
         return [float(element) for element in vars.split()], float(rhs)
 
     def slack(self):
-        sense = re.findall("[<>]*=", self._expression)[0]
+        sense = re.findall("[<>]?=", self._expression)[0]
         if sense == '<=':
             return 1
         elif sense == '>=':
@@ -26,16 +26,16 @@ class Constraint:
 
     def component(self, element="body"):
         if element == "body":
-            return self.vector()[0]
-        elif element == "slack":
-            return self.slack()
+            return self.vector()[0]  # lhs (e.g. 1 2 3 )
+        elif element == "rhs":
+            return  self.vector()[1]  # rhs (e.g 4)
         else:
-            return  self.vector()[1]
+            return self.slack()  # return 1, -1, 0
         
 
     
 class ModelLP:
-    def __init__(self, name='Model', objective=None):
+    def __init__(self, name="Model", objective=None):
         self._name = name
         self._bodycoeff = [] 
         self._rhs = []
@@ -50,80 +50,118 @@ class ModelLP:
         
     @property
     def name(self):
-        print(self._name, self._sense)
+        print(f"Name: {self._name} \nType: {self._sense}")
 
-    def setObjective(self, obj):
-        self._sense = obj.strip().split()[0]  # sense in first place of the list
-        coefficients = obj.strip().split()[1: ]
+    def _setObjective(self, expression):
+        obj_components = expression.strip().split()  # component of an objective function (sense, coefficients)
+        self._sense = obj_components[0]  # sense in first place of the list (max or min)
+        coefficients = obj_components[1: ]  # only numbers (objective function coefficients)
         self._objective = np.array(coefficients, dtype=np.float)
+        if self._num_const > 0:
+            self._num_const -= 1
+        
 
-
+    def load_instance(self, string_expressions):
+        'Strings with instance semi colon separated'
+        expression_list = string_expressions.split(";")
+        for expression in expression_list:
+            self.addExpr(expression)
+                  
+        
     def getObjective(self):
         return self._objective
         
 
-    def addConstr(self, expression):
+    def addExpr(self, expression):
         self._num_const += 1
-        constr = Constraint(expression)
-        self._bodycoeff.append(constr.component("body"))
-        self._slacks.append(constr.component("slack"))
-        self._rhs.append(constr.component("rhs"))
+        is_objective = re.match("^m(ax|in)", expression.lstrip())  # match min or max 
+        if is_objective:
+            self._setObjective(expression.strip())
+        else:
+            constr = Constraint(expression.strip())
+            self._bodycoeff.append(constr.component("body"))
+            self._slacks.append(constr.component("slack")) # add -1,1 or 0 to list _rhs
+            self._rhs.append(constr.component("rhs"))  #  add rhs to _rhs list
 
         
     def get_rhs(self):
         if not self._rhs:
             print("There is no Constraints in the model")
-        self.rhs_matrix = np.array(self._rhs).reshape(self._num_const,1)
+        # self.rhs_matrix = np.array(self._rhs).reshape(self._num_const,1)
+        self.rhs_matrix = np.array(self._rhs)
         return self.rhs_matrix
 
     @property
     def num_slacks(self):
-        return (np.array(self._slacks) > 0).sum()
+        print((np.array(self._slacks) > 0).sum())
     
     def set_matrix_form(self):
         body = np.array(self._bodycoeff)
-        indx = np.where(np.array(self._slacks) == 0)[0]  # zero-value slacks variables
+        indx = np.where(np.array(self._slacks) == 0)[0]  # where there are zero-value slacks variables
         slacks = np.diag(self._slacks)
-        if indx.size > 0:  # remove slacks 0 from slack matrix
+        if indx.size > 0:  # if thera are zero-values slackas variable then remove zero-value slacks from slack matrix
             slacks = np.delete(slacks, indx, axis=1)    
-        self.body_matrix, self.slack_matrix =  body, slacks
+        self.body_matrix, self.slack_matrix =  body, slacks  # create body matrix and slack matrix
+        return np.hstack([self.body_matrix, self.slack_matrix])  # return matrix
 
-    def standard_form(self):
-        self.set_matrix_form()
-        matrix = np.hstack([self.body_matrix, self.slack_matrix])
-        print(matrix)
+    # def standard_form(self):
+    #     self.set_matrix_form()  # initialize body matrix and slack matrix
+    #     matrix = np.hstack([self.body_matrix, self.slack_matrix])
+    #     print(matrix)
         
 
-    def get_submatrices(self):
+        
+    def get_solutions(self):
         'Matrix generator from original coefficient matrix'
-        self.set_matrix_form()
-        matrix = np.hstack([self.body_matrix, self.slack_matrix])
+        feasibles = []
+        infeasibles = []
+        if self.slack_matrix:  # zero-values slack variables
+            objective = np.concatenate([self._objective, np.zeros_like(self._slacks)])  # create objective function with slacks variables
+        else:
+            objective = self._objective
+        matrix = self.set_matrix_form()  # matrix ensamble ==> body + slacks
         numconstr, numvars = matrix.shape
         assert numconstr == self._num_const, "Number of constraints are different"
-        return (matrix[:,[*columns]] for columns in combinations(range(numvars), numconstr))
+        b = self.get_rhs()  # get rhs vector
+        for columns in combinations(range(numvars), numconstr):
+            solution = np.zeros(objective.size)
+            m = matrix[:, [*columns]] # submatrix
+            try:
+                basic = np.linalg.solve(m, b)
+                solution[[*columns]] = basic
+                if np.all(basic >= 0):   # feasible solution
+                    z = objective.dot(solution)
+                    feasibles.append(np.concatenate([solution, [z]]))
+                else:
+                    infeasibles.append(solution)
+            except np.linalg.LinAlgError:
+                print("Singular Matrix")
+        feasibles = np.array(feasibles)
+        infeasibles = np.array(infeasibles)
+        print(feasibles)
+        print()
+        print(infeasibles)
+            
+            
 
-    def invmats(self):
-        mats = self.get_submatrices()
-        return (np.linalg.inv(m) for m in mats)
 
     def get_basics(self):
         'Solve equations'
         b = self.get_rhs()
-        inverses = self.invmats()
-        return (Ainv.dot(b) for Ainv in inverses)
+        matrices = self.get_submatrices()
+        print( list((np.linalg.solve(mat, b) for mat in matrices)))  # Basic solutions
 
     
     
     def zvalues(self):
-        objective = np.array(self._objective)
-        objective = np.concatenate([objective, np.zeros_like(self._slacks)])
-        basics = self.get_basics()  # generator
+        objective = np.concatenate([self._objective, np.zeros_like(self._slacks)])  # create objective function with slacks variables
+        basics = self.get_basics()  # generator. Solutions
         positions = combinations(range(objective.size), self._num_const)
         zvalues = []
         solutions = []
         for basic,position in zip(basics, positions):
-            solution = np.zeros((objective.size,1))
-            solution[list(position)] = basic
+            solution = np.zeros((objective.size,1))  # column vector
+            solution[list(position)] = basic  # set basic solution in positions
             solutions.append(solution.flatten())
             z = objective.dot(solution)
             zvalues.append(z[0])
@@ -135,13 +173,6 @@ class ModelLP:
         #print(np.hstack([x, z.reshape(len(x), 1)]))
         
         
-    def solve(self):
-        A = np.hstack([self.body_matrix, self.slack_matrix])
-        print(list(self.get_basics(A)))
-        
-    def coefficient_matrix(self):
-        self.set_matrix_form()
-        print(np.hstack([self.body_matrix, self.slack_matrix]))
         
     def show_constr(self):
         print(self._bodycoeff)
@@ -181,12 +212,10 @@ class LP:
 
 
 if __name__ == "__main__":
-    model = ModelLP("Example 2.15-2")
-    model.addConstr("5 4 2 1 = 100")
-    model.addConstr("2 3 8 1  = 75")
-    model.setObjective("max 12 8 14 10")
-    model.standard_form()
-    #model.zvalues()
+    model = ModelLP("Example 2.15-3")
+    model.load_instance("5 4 2 1 = 100; 2 3 8 1  = 75; max 12 8 14 10")
     model.name
-
-
+    model.get_solutions()
+    #model2 = ModelLP("Example 2.17-1")
+    #model2.load_instance("min 12 20; 6 8 >= 100; 7 12 >= 120")
+    #model2.standard_form()
